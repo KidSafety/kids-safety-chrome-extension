086f99d30e9989e1e8b341a3d1948b7c3ad1f781
khoavn02 <khoavungoc02@gmail.com>
2024-04-03 22:49:19 +0700

    fetch webhistory

--- components/Shared/MonitoringRow.tsx
+++ components/Shared/MonitoringRow.tsx
@@ -1,3 +1,4 @@
+import dayjs from "dayjs"
 import React from "react"
 
 import EyeTick from "~components/Icons/EyeTick"
@@ -5,12 +6,17 @@ import WarnRed from "~components/Icons/WarnRed"
 
 type Props = {
   url: string
-  type: string
+  category: string
   timeStamp: string
   isBorder: boolean
 }
 
-function MonitoringRow({ timeStamp, type, url, isBorder }: Props) {
+function MonitoringRow({
+  timeStamp,
+  category,
+  url,
+  isBorder
+}: Readonly<Props>) {
   return (
     <div
       className={`w-full grid grid-cols-[.5fr,3fr,2fr,2fr,2fr] px-3 ${isBorder && "border-b-[1px] border-[#D1E3FE]"}`}>
@@ -22,12 +28,16 @@ function MonitoringRow({ timeStamp, type, url, isBorder }: Props) {
           id=""
         />
       </div>
-      <div className="flex justify-start item-center py-4">{url}</div>
-      <div className="flex justify-start item-center py-4">
-        <p className="text-[16px] text-[#333333] text-left">{type}</p>
+      <div className="flex justify-start items-center py-4 mr-4 overflow-x-auto whitespace-no-wrap">
+        {url}
       </div>
       <div className="flex justify-start item-center py-4">
-        <p className="text-[16px] text-[#333333] text-left">{timeStamp}</p>
+        <p className="text-[16px] text-[#333333] text-left">{category}</p>
+      </div>
+      <div className="flex justify-start item-center py-4">
+        <p className="text-[16px] text-[#333333] text-left">
+          {dayjs(timeStamp).format("YYYY/MM/DD HH:mm:ss")}
+        </p>
       </div>
       <div className="flex justify-start item-center py-4 gap-3">
         <button>
--- lib/webhistory/WebHistoryService.ts
+++ lib/webhistory/WebHistoryService.ts
@@ -21,7 +21,7 @@ class WebHistoryService {
    * Currenly it returns the last 100 history items
    * @returns Promise<chrome.history.HistoryItem[]>
    */
-  public getHistory = async (): Promise<chrome.history.HistoryItem[]> => {
+  getHistory = async (): Promise<chrome.history.HistoryItem[]> => {
     // Todo: base on user's plan, we can change the maxResults
     // For free users, we can limit the maxResults to 100
     // For premium users, we can limit the maxResults to 1000, max 2000
@@ -31,6 +31,18 @@ class WebHistoryService {
       chrome.history.search({ text: "", maxResults }, (results) => {
         resolve(results)
       })
+    })
+  }
+
+  fetchRemoteHistory = async () => {
+    return fetch(`${BASE_URL}/api/v1/webhistory/list`, {
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then((res) => {
+      if (res.ok) return res.json()
+      console.error("Failed to fetch remote history", res.statusText)
     })
   }
 }
